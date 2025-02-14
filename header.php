e3d34b4add8795114ee08526605ff37596b8255e
Milla Kärkkäinen <karkkimilla@gmail.com>
2025-02-14 09:48:00 +0200

    Update header.php

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Karels Oy</title>
+    <title>Esimerkki Oy</title>
     <link rel="icon" type="image/x-icon" href="images/faviconVarillinen.png">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
